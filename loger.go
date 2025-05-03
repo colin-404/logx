8c65078668f631653a461cd2f766f94a9382987d
wenxi-3000 <1712472820@qq.com>
2025-05-03 21:17:52 +0800

    update

---
+++ loger.go
@@ -0,0 +1,141 @@
+package loger
+
+import (
+	"fmt"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+const (
+	defaultLogFile    = "./default.log"
+	defaultMaxSize    = 5
+	defaultMaxAge     = 3
+	defaultMaxBackups = 3
+)
+
+var defaultLogger *Loger
+
+func InitLogger(logger *Loger) {
+	defaultLogger = logger
+}
+
+// Logger
+type Loger struct {
+	provider *zap.Logger
+	msg      string
+	lvl      int
+}
+
+func NewLoger(opts ...interface{}) *Loger {
+	var (
+		opt     interface{}
+		options *Options
+	)
+
+	options = &Options{}
+	for _, opt = range opts {
+		opt.(Option)(options)
+	}
+
+	logFile := defaultLogFile
+	if options.LogFile != "" {
+		logFile = options.LogFile
+	}
+	maxSize := defaultMaxSize
+	if options.MaxSize != 0 {
+		maxSize = options.MaxSize
+	}
+	maxAge := defaultMaxAge
+	if options.MaxAge != 0 {
+		maxAge = options.MaxAge
+	}
+	maxBackups := defaultMaxBackups
+	if options.MaxSize != 0 {
+		maxBackups = options.MaxBackups
+	}
+	logLevel := options.Level
+
+	hook := &lumberjack.Logger{
+		Filename:   logFile,
+		MaxSize:    maxSize,
+		MaxAge:     maxAge,
+		MaxBackups: maxBackups,
+		LocalTime:  false,
+		Compress:   false,
+	}
+
+	core := zapcore.NewCore(zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout),
+			zapcore.AddSync(hook)),
+		zapcore.Level(logLevel))
+	return &Loger{provider: zap.New(core), lvl: logLevel}
+}
+
+func (l *Loger) SetMsg(msg string) {
+	l.msg = msg
+}
+
+func (l *Loger) Print(v ...interface{}) {
+	l.provider.Info(l.msg, zap.Any("info", v))
+}
+
+func (l *Loger) Printf(format string, v ...interface{}) {
+	l.provider.Info(l.msg, zap.Any("info", fmt.Sprintf(format, v...)))
+}
+
+func (l *Loger) Println(v ...interface{}) {
+	l.provider.Info(l.msg, zap.Any("info", v))
+}
+
+func Fatalf(msg string, format string, v ...interface{}) {
+	if defaultLogger == nil {
+		fmt.Printf(format+"\n", v...)
+		return
+	}
+	if defaultLogger.lvl <= FatalLevel {
+		defaultLogger.provider.Fatal(msg, zap.String("info", fmt.Sprintf(format, v...)))
+	}
+}
+
+func Errorf(msg string, format string, v ...interface{}) {
+	if defaultLogger == nil {
+		fmt.Printf(format+"\n", v...)
+		return
+	}
+	if defaultLogger.lvl <= ErrorLevel {
+		defaultLogger.provider.Error(msg, zap.String("info", fmt.Sprintf(format, v...)))
+	}
+}
+
+func Warnf(msg string, format string, v ...interface{}) {
+	if defaultLogger == nil {
+		fmt.Printf(format+"\n", v...)
+		return
+	}
+	if defaultLogger.lvl <= WarnLevel {
+		defaultLogger.provider.Warn(msg, zap.String("info", fmt.Sprintf(format, v...)))
+	}
+}
+
+func Infof(msg string, format string, v ...interface{}) {
+	if defaultLogger == nil {
+		fmt.Printf(format+"\n", v...)
+		return
+	}
+	if defaultLogger.lvl <= InfoLevel {
+		defaultLogger.provider.Info(msg, zap.String("info", fmt.Sprintf(format, v...)))
+	}
+}
+
+func Debugf(msg string, format string, v ...interface{}) {
+	if defaultLogger == nil {
+		fmt.Printf(format+"\n", v...)
+		return
+	}
+	if defaultLogger.lvl <= DebugLevel {
+		defaultLogger.provider.Debug(msg, zap.String("info", fmt.Sprintf(format, v...)))
+	}
+}
